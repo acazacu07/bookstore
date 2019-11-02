e35eda42b71d35d7d07ae7c8b7a3236f633a6463
EC2 Default User <ec2-user@ip-172-31-18-10.ec2.internal>
2019-11-02 01:08:23 +0000

    bookstore mvc

---
+++ db/migrate/20191101233655_create_adverts.rb
@@ -0,0 +1,12 @@
+class CreateAdverts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :adverts do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.integer :seller_id
+
+      t.timestamps
+    end
+  end
+end
